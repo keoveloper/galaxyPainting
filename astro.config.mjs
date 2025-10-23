f3868379afa16ca9f257586fcae2df1f68fce62b
keobandolero <octav0773@gmail.com>
2025-10-23 10:01:24 -0600

    cambios restablecidos

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://keoveloper.github.io",
-  base: "/galaxyPainting",
   integrations: [tailwind(), react()],
 });
